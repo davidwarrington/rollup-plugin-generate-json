d4086bfaa6df8b0f040dfeee070615ccb609d454
David Warrington <davidmwarrington+github@gmail.com>
2021-11-26 00:04:05 +0000

    feat(test): delete generated files after run

--- tests/run.spec.js
+++ tests/run.spec.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { expect, test } = require('@jest/globals');
+const { afterAll, expect, test } = require('@jest/globals');
+const glob = require('glob');
 
 const baseDir = './tests';
 const isDir = filepath => {
@@ -11,15 +12,19 @@ const isDir = filepath => {
 const tests = fs
     .readdirSync(baseDir)
     .filter(testDir => isDir(path.join(baseDir, testDir)))
-    .map(testDir => {
-        return [
-            testDir,
-            /* eslint-disable import/no-dynamic-require, global-require */
-            require(`./${testDir}/expected.json`),
-            require(`./${testDir}/dist/output.json`),
-            /* eslint-enable import/no-dynamic-require, global-require */
-        ];
-    });
+    .map(testDir => [
+        testDir,
+        /* eslint-disable import/no-dynamic-require, global-require */
+        require(`./${testDir}/expected.json`),
+        require(`./${testDir}/dist/output.json`),
+        /* eslint-enable import/no-dynamic-require, global-require */
+    ]);
+
+afterAll(() => {
+    const testNames = tests.map(([name]) => name);
+    const pattern = `${baseDir}/@(${testNames.join('|')})/dist/`;
+    glob.sync(pattern).forEach(dir => fs.rmSync(dir, { recursive: true }));
+});
 
 test.each(tests)(`%p generates the expected result`, (_, expected, output) => {
     expect(expected).toStrictEqual(output);
